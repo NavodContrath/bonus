250a5803f2d085377fde477042a5d66a9a4fe860
NavodContrath <navod.contrath@gmail.com>
2025-02-11 17:58:00 +0100

    fix code indentation

--- main.js
+++ main.js
@@ -14,10 +14,8 @@ tools used
 */
 const userChoice = prompt("choose even or uneven [even, uneven]")
 console.log(userChoice);
-
 const userNumber = Number(prompt("choose a number between 1 and 9"))
 console.log(userNumber);
-
 //Generare un numero casuale compreso tra 1 e 9, che sarà la giocata del computer.
 const pcNumber = Math.floor(Math.random() * 9) + 1;
 console.log(pcNumber);
@@ -28,29 +26,21 @@ console.log(sum);
 if (sum % 2 === 0 && userChoice === "even") {
     let evenWins = "U Win!"
     alert(evenWins);
-
-}
-else if (sum % 2 !== 0 && userChoice === "even") {
+} else if (sum % 2 !== 0 && userChoice === "even") {
     let evenLost = "U Lost!"
     alert(evenLost);
-
-}
-else if (sum % 2 !== 0 && userChoice === "uneven") {
+} else if (sum % 2 !== 0 && userChoice === "uneven") {
     let unevenWins = "U Win!"
     alert(unevenWins)
-}
-else if (sum % 2 === 0 && userChoice === "uneven") {
+} else if (sum % 2 === 0 && userChoice === "uneven") {
     let unevenLost = "U Lost!"
     alert(unevenLost);
 }
 
 /* quanto manca:  Stabilire l'ora attuale e attraverso una operazione matematica stabilire i minuti mancanti allo scoccare della prossima ora.  Oltre a Math ci sono anche altre funzionalitá di javascript (built-in) che possaimo usare senza installazioni come ad esempio le funzioni dell'oggetto Date. */
 
-
 const oraMinuti = 60
-
 const now = new Date();
 console.log(oraMinuti - now.getMinutes());
-
 const remaining = oraMinuti - now.getMinutes()
 console.log(`remaining time of the hour ${remaining} minutes`);
